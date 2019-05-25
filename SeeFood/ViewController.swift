0ed932b28a656a190e707300e4bd1627175b2789
Jeremy Adam <underway113@gmail.com>
2019-05-26 00:38:49 +0700

    Adjust Auto Layout + add UI Picker View for user select model ML

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -10,23 +10,38 @@ import UIKit
 import CoreML
 import Vision
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
     
     @IBOutlet weak var ImageView: UIImageView!
+    @IBOutlet weak var pickerViewModel: UIPickerView!
     
-    //Collection Variable Display    
-    
+    //Collection Variable Results Display
     @IBOutlet var txtObjectCollection: [UILabel]!
     @IBOutlet var txtConfCollection: [UILabel]!
     
     let imagePicker = UIImagePickerController()
     
+    var modelSelected:MLModel = MLModel()
+    let modelCollection:[String:MLModel] =
+        [
+            "Inceptionv3":Inceptionv3().model,
+            "Cat vs Dog":cat_dog_20iter_98_95eval_1().model
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        pickerViewModel.delegate = self
+        pickerViewModel.dataSource = self
+    
         
         imagePicker.delegate = self
         imagePicker.sourceType = .camera
         imagePicker.allowsEditing = false
+        
+        
+        //Default Value of ML Model
+        modelSelected = [MLModel](modelCollection.values)[0]
         
     }
     
@@ -34,6 +49,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         present(imagePicker, animated: true, completion: nil)
     }
     
+    //UI Image Picker
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let userPickedimage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             ImageView.image = userPickedimage
@@ -47,17 +63,33 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         imagePicker.dismiss(animated: true, completion: nil)
     }
+    //
     
     
+    //UI Picker View
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return modelCollection.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return [String](modelCollection.keys)[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 30
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        modelSelected = [MLModel](modelCollection.values)[row]
+    }
+    //
+    
+    //Detect Picture
     func detect(_ image:CIImage) {
-        
-        //Using Model Inceptionv3
-//        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
-//            fatalError("Loading CoreML Model Failed")
-//        }
+        print(modelSelected)
         
         //Using CreateML Model Cat vs Dog
-        guard let model = try? VNCoreMLModel(for: cat_dog_20iter_98_95eval_1().model) else {
+        guard let model = try? VNCoreMLModel(for: modelSelected) else {
             fatalError("Loading CoreML Model Failed")
         }
 
@@ -86,6 +118,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             print(error)
         }
     }
+    //
 
 
 }
